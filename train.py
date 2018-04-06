621ebe7b8f5ad517a7113a41aaed794d513bd398
iver56 <ij@aiascience.com>
2018-04-06 12:26:28 +0200

    Tweak training script

--- train.py
+++ train.py
@@ -6,9 +6,6 @@ def parse_args(args):
     subparsers = parser.add_subparsers(help='Arguments for specific dataset types.', dest='dataset_type')
     subparsers.required = True
 
-    def csv_list(string):
-        return string.split(',')
-
     csv_parser = subparsers.add_parser('csv')
     csv_parser.add_argument('--annotations', help='Path to CSV file containing annotations for training.', default=os.path.join('data', 'training', 'annotations.csv'))
     csv_parser.add_argument('--classes', help='Path to a CSV file containing class label mapping.', default=os.path.join('data', 'training', 'classes.csv'))
@@ -25,8 +22,8 @@ def parse_args(args):
     parser.add_argument('--gpu',             help='Id of the GPU to use (as reported by nvidia-smi).')
     parser.add_argument('--multi-gpu',       help='Number of GPUs to use for parallel processing.', type=int, default=0)
     parser.add_argument('--multi-gpu-force', help='Extra flag needed to enable (experimental) multi-gpu support.', action='store_true')
-    parser.add_argument('--epochs',          help='Number of epochs to train.', type=int, default=25)
-    parser.add_argument('--steps',           help='Number of steps per epoch.', type=int, default=50)
+    parser.add_argument('--epochs',          help='Number of epochs to train.', type=int, default=30)
+    parser.add_argument('--steps',           help='Number of steps per epoch.', type=int, default=100)
     parser.add_argument('--snapshot-path',   help='Path to store snapshots of models during training', default=os.path.join('data', 'model_snapshots'))
     parser.add_argument('--tensorboard-dir', help='Log directory for Tensorboard output', default=os.path.join('data', 'tensorboard_logs'))
     parser.add_argument('--no-snapshots',    help='Disable saving snapshots.', dest='snapshots', action='store_false')
