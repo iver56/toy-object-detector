d8d08b5dc24211df042922af95193ede9907d3ed
iver56 <ij@aiascience.com>
2018-04-06 11:23:08 +0200

    Add training script, based on keras-retinanet

---
+++ train.py
@@ -0,0 +1,135 @@
+from keras_retinanet.bin.train import *
+
+
+def parse_args(args):
+    parser     = argparse.ArgumentParser(description='Simple training script for training a RetinaNet network.')
+    subparsers = parser.add_subparsers(help='Arguments for specific dataset types.', dest='dataset_type')
+    subparsers.required = True
+
+    coco_parser = subparsers.add_parser('coco')
+    coco_parser.add_argument('coco_path', help='Path to dataset directory (ie. /tmp/COCO).')
+
+    pascal_parser = subparsers.add_parser('pascal')
+    pascal_parser.add_argument('pascal_path', help='Path to dataset directory (ie. /tmp/VOCdevkit).')
+
+    kitti_parser = subparsers.add_parser('kitti')
+    kitti_parser.add_argument('kitti_path', help='Path to dataset directory (ie. /tmp/kitti).')
+
+    def csv_list(string):
+        return string.split(',')
+
+    oid_parser = subparsers.add_parser('oid')
+    oid_parser.add_argument('main_dir', help='Path to dataset directory.', default=os.path.join('data', 'training'))
+    oid_parser.add_argument('--version',  help='The current dataset version is V3.', default='2017_11')
+    oid_parser.add_argument('--labels-filter',  help='A list of labels to filter.', type=csv_list, default=None)
+    oid_parser.add_argument('--annotation-cache-dir', help='Path to store annotation cache.', default='.')
+    oid_parser.add_argument('--fixed-labels', help='Use the exact specified labels.', default=False)
+
+    csv_parser = subparsers.add_parser('csv')
+    csv_parser.add_argument('--annotations', help='Path to CSV file containing annotations for training.', default=os.path.join('data', 'training', 'annotations.csv'))
+    csv_parser.add_argument('--classes', help='Path to a CSV file containing class label mapping.', default=os.path.join('data', 'training', 'classes.csv'))
+    csv_parser.add_argument('--val-annotations', help='Path to CSV file containing annotations for validation (optional).', default=os.path.join('data', 'validation', 'annotations.csv'))
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--snapshot',          help='Resume training from a snapshot.')
+    group.add_argument('--imagenet-weights',  help='Initialize the model with pretrained imagenet weights. This is the default behaviour.', action='store_const', const=True, default=True)
+    group.add_argument('--weights',           help='Initialize the model with weights from a file.')
+    group.add_argument('--no-weights',        help='Don\'t initialize the model with any weights.', dest='imagenet_weights', action='store_const', const=False)
+
+    parser.add_argument('--backbone',        help='Backbone model used by retinanet.', default='resnet50', type=str)
+    parser.add_argument('--batch-size',      help='Size of the batches.', default=1, type=int)
+    parser.add_argument('--gpu',             help='Id of the GPU to use (as reported by nvidia-smi).')
+    parser.add_argument('--multi-gpu',       help='Number of GPUs to use for parallel processing.', type=int, default=0)
+    parser.add_argument('--multi-gpu-force', help='Extra flag needed to enable (experimental) multi-gpu support.', action='store_true')
+    parser.add_argument('--epochs',          help='Number of epochs to train.', type=int, default=25)
+    parser.add_argument('--steps',           help='Number of steps per epoch.', type=int, default=50)
+    parser.add_argument('--snapshot-path',   help='Path to store snapshots of models during training', default=os.path.join('data', 'model_snapshots'))
+    parser.add_argument('--tensorboard-dir', help='Log directory for Tensorboard output', default=os.path.join('data', 'tensorboard_logs'))
+    parser.add_argument('--no-snapshots',    help='Disable saving snapshots.', dest='snapshots', action='store_false')
+    parser.add_argument('--no-evaluation',   help='Disable per epoch evaluation.', dest='evaluation', action='store_false')
+    parser.add_argument('--freeze-backbone', help='Freeze training of backbone layers.', action='store_true')
+
+    return check_args(parser.parse_args(args))
+
+
+def main(args=None):
+    # parse arguments
+    if args is None:
+        args = sys.argv[1:]
+    args = parse_args(args)
+
+    # make sure keras is the minimum required version
+    check_keras_version()
+
+    # optionally choose specific GPU
+    if args.gpu:
+        os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+    keras.backend.tensorflow_backend.set_session(get_session())
+
+    # create the generators
+    train_generator, validation_generator = create_generators(args)
+
+    if 'resnet' in args.backbone:
+        from keras_retinanet.models.resnet import resnet_retinanet as retinanet, custom_objects, download_imagenet
+    elif 'mobilenet' in args.backbone:
+        from keras_retinanet.models.mobilenet import mobilenet_retinanet as retinanet, custom_objects, download_imagenet
+    elif 'vgg' in args.backbone:
+        from keras_retinanet.models.vgg import vgg_retinanet as retinanet, custom_objects, download_imagenet
+    elif 'densenet' in args.backbone:
+        from keras_retinanet.models.densenet import densenet_retinanet as retinanet, custom_objects, download_imagenet
+    else:
+        raise NotImplementedError('Backbone \'{}\' not implemented.'.format(args.backbone))
+
+    # create the model
+    if args.snapshot is not None:
+        print('Loading model, this may take a second...')
+        model            = keras.models.load_model(args.snapshot, custom_objects=custom_objects)
+        training_model   = model
+        prediction_model = model
+    else:
+        weights = args.weights
+        # default to imagenet if nothing else is specified
+        if weights is None and args.imagenet_weights:
+            weights = download_imagenet(args.backbone)
+
+        print('Creating model, this may take a second...')
+        model, training_model, prediction_model = create_models(
+            backbone_retinanet=retinanet,
+            backbone=args.backbone,
+            num_classes=train_generator.num_classes(),
+            weights=weights,
+            multi_gpu=args.multi_gpu,
+            freeze_backbone=args.freeze_backbone
+        )
+
+    # print model summary
+    print(model.summary())
+
+    # this lets the generator compute backbone layer shapes using the actual backbone model
+    if 'vgg' in args.backbone or 'densenet' in args.backbone:
+        compute_anchor_targets = functools.partial(anchor_targets_bbox, shapes_callback=make_shapes_callback(model))
+        train_generator.compute_anchor_targets = compute_anchor_targets
+        if validation_generator is not None:
+            validation_generator.compute_anchor_targets = compute_anchor_targets
+
+    # create the callbacks
+    callbacks = create_callbacks(
+        model,
+        training_model,
+        prediction_model,
+        validation_generator,
+        args,
+    )
+
+    # start training
+    training_model.fit_generator(
+        generator=train_generator,
+        steps_per_epoch=args.steps,
+        epochs=args.epochs,
+        verbose=1,
+        callbacks=callbacks,
+    )
+
+
+if __name__ == '__main__':
+    main()
